bcda7fa9b60006b642427ee95c6180f7554b993a
nguyenston <phucnguyen1999.nn@gmail.com>
2023-12-10 02:04:46 -0500

    first custom modification

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,19 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local set = vim.keymap.set
+local del = vim.keymap.del
+
+set("n", "<leader>w", "<cmd>w<cr><esc>", { desc = "Save file" })
+
+-- remaping windows-related commands
+set("n", "<leader>mo", "<cmd>only<cr>", { desc = "Delete other windows" })
+set("n", "<leader>md", "<cmd>close<cr>", { desc = "Delete window" })
+set("n", "<leader>ms", "<cmd>split<cr>", { desc = "Split window below" })
+set("n", "<leader>mv", "<cmd>vsplit<cr>", { desc = "Split window right" })
+
+del("n", "<leader>ww")
+del("n", "<leader>wd")
+del("n", "<leader>w-")
+del("n", "<leader>w|")
---
+++ lua/plugins/core.lua
@@ -0,0 +1,13 @@
+return {
+  -- Make tokyonight transparent
+  {
+    "folke/tokyonight.nvim",
+    opts = {
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+    },
+  }
+}
