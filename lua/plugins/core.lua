b579cea0c358a2ff0d0aba76d443b74581d31691
nguyenston <phucnguyen1999.nn@gmail.com>
2023-12-12 01:27:01 -0500

    make lsp servers work on nixos

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -14,6 +14,7 @@ return {
           },
         },
         { name = "nvim_lsp" },
+        { name = "nvim_lsp_signature_help" },
         { name = "luasnip" },
         { name = "path" },
       }, {
@@ -106,53 +107,31 @@ return {
   {
     "neovim/nvim-lspconfig",
     opts = function(_, opts)
-      opts.servers = {}
-    end,
-  },
-
-  -- set up lspconfig without mason
-  {
-    "VonHeikemen/lsp-zero.nvim",
-    branch = "v3.x",
-    init = function(_)
-      local lsp_zero = require("lsp-zero")
-      lsp_zero.extend_lspconfig()
-      lsp_zero.on_attach(function(_, bufnr)
-        -- see :help lsp-zero-keybindings
-        -- to learn the available actions
-        lsp_zero.default_keymaps({ buffer = bufnr })
-      end)
-
-      require("lspconfig").lua_ls.setup({
-        on_init = function(client)
-          local path = client.workspace_folders[1].name
-          if not vim.loop.fs_stat(path .. "/.luarc.json") and not vim.loop.fs_stat(path .. "/.luarc.jsonc") then
-            client.config.settings = vim.tbl_deep_extend("force", client.config.settings, {
-              Lua = {
-                runtime = {
-                  -- Tell the language server which version of Lua you're using
-                  -- (most likely LuaJIT in the case of Neovim)
-                  version = "LuaJIT",
-                },
-                -- Make the server aware of Neovim runtime files
-                workspace = {
-                  checkThirdParty = false,
-                  library = {
-                    vim.env.VIMRUNTIME,
-                    -- "${3rd}/luv/library"
-                    -- "${3rd}/busted/library",
-                  },
-                  -- or pull in all of 'runtimepath'. NOTE: this is a lot slower
-                  -- library = vim.api.nvim_get_runtime_file("", true)
-                },
+      opts.servers = {
+        rust_analyzer = { mason = false },
+        lua_ls = {
+          mason = false, -- set to false if you don't want this server to be installed with mason
+          -- Use this to add any additional keymaps
+          -- for specific lsp servers
+          ---@type LazyKeysSpec[]
+          -- keys = {},
+          settings = {
+            Lua = {
+              runtime = {
+                -- Tell the language server which version of Lua you're using
+                -- (most likely LuaJIT in the case of Neovim)
+                version = "LuaJIT",
               },
-            })
-            client.notify("workspace/didChangeConfiguration", { settings = client.config.settings })
-          end
-          return true
-        end,
-      })
-      require("lspconfig").rust_analyzer.setup({})
+              workspace = {
+                checkThirdParty = false,
+              },
+              completion = {
+                callSnippet = "Replace",
+              },
+            },
+          },
+        },
+      }
     end,
   },
 }
